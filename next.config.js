c3861f0d6e4df59e2bc48e2f0547e0997c2c1e2a
DESKTOP-2MMM000\Alexandre Santos <alexandre.santos@crptecnologia.com.br>
2023-03-15 14:36:54 -0300

    fix.004-Buildin the project

--- next.config.js
+++ next.config.js
@@ -3,16 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/github",
-        destination: "https://github.com/steven-tey/precedent",
-        permanent: false,
-      },
-    ];
+    loader: 'imgix',
+    path: '/',
   },
 };
 

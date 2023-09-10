c83a107f39ee896ba0afe63afa855f245124156c
unknown <alexandreifto2@gmail.com>
2023-09-10 14:56:28 -0300

    fix:label_title_initial

--- components/Banner/HomeBanner.js
+++ components/Banner/HomeBanner.js
@@ -21,7 +21,7 @@ export const HomeBanner = () => {
                 {/* Appear Second */}
                 <MotionBTTContainer transition={{ delay: 0.4, duration: 0.5 }}>
                     <PageTitle className="text-center mx-auto" type="heavy">
-                        Transforme Clientes em Fãs! Nosso sistema vai fazer você amar sua própria empresa. Descubra como no botão abaixo.
+                        Transforme Clientes em Fãs! Nosso sistema vai fazer o cliente amar a sua empresa.
                     </PageTitle>
                 </MotionBTTContainer>
                 {/* Appear Third */}
